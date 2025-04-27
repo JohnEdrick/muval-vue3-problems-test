e683b636e20d1e46565aa5e7a1d08b77eb2b4dfc
EdrickNubla <john.nubla@rsb-consulting.com>
2025-04-27 12:50:36 +0800

    Refactor useCounter to utilize toRefs for reactivity and implement Pinia store for counter management

--- src/composables/useCounter.ts
+++ src/composables/useCounter.ts
@@ -1,11 +1,15 @@
-import { reactive } from 'vue'
+import { reactive, toRefs } from 'vue'
 import type { MaybeRef } from 'vue'
 
 export function useCounter(initial_value: MaybeRef<number> = 0) {
   const data = reactive({
     count: initial_value,
   })
+
   const increment = () => data.count++
 
-  return { ...data, increment }
+  return { ...toRefs(data), increment }
+  //Spreading reactive objects breaks reactivity.
+  // So we use toRefs to convert the reactive object into a plain object with refs.
+  // This way, the properties of the object will still be reactive when used in the template or in other parts of the code.
 }
